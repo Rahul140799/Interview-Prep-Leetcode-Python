c4352fedd15f0c42fc8893d5a27b9a68eaf6607e
Rahul140799 <43520919+Rahul140799@users.noreply.github.com>
2022-02-02 02:11:06 +0530

    Time: 109 ms (51.91%), Space: 15.2 MB (97.83%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.py
+++ 261-graph-valid-tree/261-graph-valid-tree.py
@@ -5,7 +5,7 @@ class UnionFind:
         
     def find(self, node):
         if self.parent[node] != node:
-            self.parent[node] = self.find(self.parent[node])
+            self.parent[node] = self.find(self.parent[self.parent[node]])
         return self.parent[node]
     
     def union(self, x, y):
