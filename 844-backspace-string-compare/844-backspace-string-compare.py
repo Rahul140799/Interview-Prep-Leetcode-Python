8a754d1f8021b002c902898065fee8b13278cf31
Rahul140799 <43520919+Rahul140799@users.noreply.github.com>
2022-05-01 11:59:29 +0530

    Time: 37 ms (66.69%), Space: 13.9 MB (25.09%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,8 +1,5 @@
 class Solution:
-    def backspaceCompare(self, s: str, t: str) -> bool:
-        
-        def compute_string(st, stack, it):
-            
+    def compute_string(self, st: str, stack: list, it: int) -> list:
             while it < len(st):
                 if stack and st[it] == '#':
                     stack.pop()
@@ -10,7 +7,7 @@ class Solution:
                     if st[it] != '#':
                         stack.append(st[it])
                 it += 1
-            # print(stack)
             return ''.join(stack)
         
-        return compute_string(s, [], 0) == compute_string(t, [], 0)
+    def backspaceCompare(self, s: str, t: str) -> bool:          
+        return self.compute_string(s, [], 0) == self.compute_string(t, [], 0)
