29f99315ff786e7c71abf8c5e0f65a34357de591
Rahul140799 <43520919+Rahul140799@users.noreply.github.com>
2022-11-15 19:46:50 +0530

    Time: 205 ms (24.52%), Space: 21.4 MB (46.74%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -6,16 +6,16 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        def recurse(root, count):
+        def recurse(root):
             if not root:
                 return 0
             
             left = 0
             right = 0
             
-            left = recurse(root.left, count+1)
-            right = recurse(root.right, count+1)
+            left = recurse(root.left)
+            right = recurse(root.right)
             
             return left + right + 1
         
-        return recurse(root, 0)
+        return recurse(root)
